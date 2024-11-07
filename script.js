60d074b3dab611b0d0cc7ee16e567ad785e5b077
Mike <39208141+MikeG3@users.noreply.github.com>
2024-11-07 12:58:32 -0500

    Add files via upload
    Get this website started

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example function to handle language switching
+/*
+document.querySelector('#english').addEventListener('click', () => {
+    alert("Switched to English");
+});
+document.querySelector('#french').addEventListener('click', () => {
+    alert("Switched to French");
+});
+*/
